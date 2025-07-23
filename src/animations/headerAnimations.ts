6b061380611b1031477357b4c37cc80d78b73a0c
work-boy <johhnythepogi@gmail.com>
2025-07-23 19:09:31 +0800

    removed some files, updated flow

--- src/animations/headerAnimations.ts
+++ src/animations/headerAnimations.ts
@@ -4,32 +4,9 @@ export function headerAnimation() {
 		defaults: { duration: 1, ease: 'power1' },
 	})
 	timeline
-		// .from('.hi', { opacity: 0, delay: 1 })
-		// .from('.im', { opacity: 0 })
-		// .from('.name', {
-		// 	opacity: 0,
-		// 	y: '-20%',
-		// 	duration: 0.5,
-		// 	ease: 'power3.out',
-		// })
-		// .to('.description span', {
-		// 	y: 0,
-		// 	opacity: 1,
-		// 	duration: 0.3,
-		// 	stagger: 0.05,
-		// 	repeat: 0,
-		// 	ease: 'power2.out',
-		// }).from('.katakana', {
-		// 	opacity: 0,
-		// 	duration: 0.3,
-		// 	ease: 'power2.out',
-		// })
-		// .to('.subtitle', {
-		// 	opacity: 1,
-		// 	y: 0,})
-		.from('header, main section',	{opacity: 0,
+		.from('header, main',	{opacity: 0,
 			y: 50,
 			ease: 'power1.out',
-			stagger: 0.3
+			stagger: 0.1
 		})
 }
